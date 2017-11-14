c38bdca0795078422b6e47d734b11b9889eb29ec
Jozef <jozef.valko.285@gmail.com>
2017-11-14 21:45:56 +0100

    RSA on CPU maybe final

--- RSA64_for_CPU/rsa64.c
+++ RSA64_for_CPU/rsa64.c
@@ -4,6 +4,8 @@
 #include <math.h>
 #include <string.h>
 
+#define MAX_KEYBOARD_INPUT 1023
+
 int debug = 0;
 
 struct public_key {
@@ -21,6 +23,7 @@ struct private_key {
 struct message {
 	unsigned char *msg;
 	unsigned long long size;
+	unsigned long long numBlocks;
 };
 
 void print_hex(char* arr, int len)
@@ -31,7 +34,7 @@ void print_hex(char* arr, int len)
 	printf("\n");
 }
 
-struct message strToInt(int bufSize, unsigned long long decrypted, int flag) {
+struct message intToStr(int bufSize, unsigned long long decrypted, int flag) {
 	struct message decMsg;
 	int i;
 	unsigned long long int temp;
@@ -185,27 +188,22 @@ void rsa_gen_keys(struct public_key *pub, struct private_key *priv, int modSize)
 	}
 }
 
-void rsa_encrypt(unsigned long long e, unsigned long long n, long long numBlocks, int bufSize, unsigned char *message, unsigned char *cipher)
+void rsa_encrypt(unsigned long long e, unsigned long long n, int bufSize, struct message message, unsigned char *cipher)
 {
-	FILE *cipher_file;
 	int i, j, flag = 0;
 	unsigned char *buf;
 	unsigned long long *encrypted;
 	clock_t begin, end;
 	double time_spent;
-	if (cipher == NULL) {
-		cipher = "cipher";
-	}
 	
-	encrypted = (unsigned long long *)malloc(numBlocks * sizeof(unsigned long long));
+	encrypted = (unsigned long long *)malloc(message.numBlocks * sizeof(unsigned long long));
 	buf = (unsigned char*)malloc(bufSize * sizeof(unsigned char));
-	cipher_file = fopen(cipher, "w");
 	begin = clock();
-	for (i = 0; i < numBlocks; i++) {
+	for (i = 0; i < message.numBlocks; i++) {
 		for (j = 0; j < bufSize; j++) {
 			if (!flag) {
-				buf[j] = *(message + i * bufSize + j);
-				if ((i == numBlocks - 1) && buf[j] == 255) {
+				buf[j] = *(message.msg + i * bufSize + j);
+				if ((i == message.numBlocks - 1) && buf[j] == 255) {
 					flag = 1;
 				}
 			}
@@ -222,28 +220,98 @@ void rsa_encrypt(unsigned long long e, unsigned long long n, long long numBlocks
 		//printf("%d. Message is %llu\n", i + 1, tempNumber);
 		encrypted[i] = rsa_modExp(encrypted[i], e, n);
 		//printf("Encrypted: %llu\n", encrypted);
-		fprintf(cipher_file, "%llu ", encrypted[i]);
+		
 	}
-	fclose(cipher_file);
+	free(buf);
 	end = clock();
 	time_spent = ((double)(end - begin) / CLOCKS_PER_SEC) * 1000;
 	if (debug) {
-		printf("Sifrovanie zabralo %lf ms.\n", time_spent);
+		printf("Sifrovanie zabralo %lf ms\n", time_spent);
 	}
-	if (debug) {
-		printf("Sifrovanie dokoncene. Sifra je ulozena v subore %s.\n", cipher);
+
+	FILE *cipher_file;
+	if (cipher != NULL) {
+		cipher_file = fopen(cipher, "wb");
 	}
-	free(buf);
+	else {
+		cipher_file = stdout;
+		if (debug) {
+			printf("Zasifrovany text:\n");
+		}
+	}
+	
+	begin = clock();
+	unsigned long long sizeWritten = fwrite(encrypted, sizeof(unsigned long long), message.numBlocks, cipher_file);
+	if (cipher != NULL) {
+		fclose(cipher_file);
+	}
+	
+	if (sizeWritten != message.numBlocks) {
+		fprintf(stderr, "[rsa_encrypt]: Nepodarilo sa ulozit sifru do suboru\n");
+		free(encrypted);
+		exit(1);
+	}
+	end = clock();
+	time_spent = ((double)(end - begin) / CLOCKS_PER_SEC) * 1000;
+	if (debug && cipher != NULL) {
+		printf("Zapis sifry na vystup trval %lf ms\n", time_spent);
+		printf("Sifrovanie dokoncene. Sifra je ulozena v subore %s\n", cipher);
+	}
+	else if(debug) {
+		printf("\nZapis sifry na vystup trval %lf ms\n", time_spent);
+		printf("Sifrovanie dokoncene. Sifra je na stdout\n");
+	}
+	free(encrypted);
 }
 
-void rsa_decrypt(unsigned long long d, unsigned long long n, int bufSize, char *cipher, char *output)
+void rsa_decrypt(unsigned long long d, unsigned long long n, int bufSize, char *input, char *output)
 {
-	unsigned long long outSize = bufSize;
-	struct message decMsg;
-	unsigned long long int *cipherMsg, *temp, decrypted, c;
-	long long numBlocks;
-	int len, i;
+	unsigned long long *decrypted;
+	unsigned long long numBlocks;
+	unsigned long long sizeOfFile;
+	int i;
+	clock_t begin, end;
+	double time_spent;
 	FILE *output_file, *cipher_file;
+	
+	begin = clock();
+	if (input != NULL) {
+		cipher_file = fopen(input, "rb");
+		fseek(cipher_file, 0, SEEK_END);
+		sizeOfFile = ftell(cipher_file);
+		rewind(cipher_file);
+		sizeOfFile /= 8;
+		decrypted = (unsigned long long *)malloc(sizeof(unsigned long long) * sizeOfFile);
+		if (!decrypted) {
+			fprintf(stderr, "[rsa_decrypt]Alokacia pamate pre decrypted zlyhala\n");
+			exit(1);
+		}
+		numBlocks = fread(decrypted, sizeof(unsigned long long), sizeOfFile, cipher_file);
+		fclose(cipher_file);
+	}
+	else {
+		freopen(NULL, "rb", stdin);
+		cipher_file = stdin;
+		fseek(cipher_file, 0, SEEK_END);
+		sizeOfFile = ftell(cipher_file);
+		rewind(cipher_file);
+		if (sizeOfFile > 0) {
+			decrypted = (unsigned long long *)malloc(sizeof(unsigned long long) * sizeOfFile);
+			numBlocks = fread(decrypted, sizeof(unsigned long long), sizeOfFile, cipher_file);
+		}
+		else {
+			fprintf(stderr, "Pokus o zadanie sifry manualne z klavesnice\n");
+			exit(1);
+		}
+	}
+	end = clock();
+	time_spent = ((double)(end - begin) / CLOCKS_PER_SEC) * 1000;
+	if (debug) {
+		printf("Nacitanie sifry zabralo %lf ms.\n", time_spent);
+		printf("Pocet blokov: %llu\n", numBlocks);
+	}
+	realloc(decrypted, numBlocks * sizeof(unsigned long long));
+
 	if (output != NULL) {
 		output_file = fopen(output, "wb");
 	}
@@ -253,59 +321,40 @@ void rsa_decrypt(unsigned long long d, unsigned long long n, int bufSize, char *
 			printf("Desifrovany text:\n");
 		}
 	}
-	cipher_file = fopen(cipher, "r");
-	decMsg.msg = (unsigned char *)malloc(sizeof(unsigned char) * bufSize);
-	cipherMsg = (unsigned long long *)malloc(sizeof(unsigned long long));
-	numBlocks = 1;
-	for (i = 0; fscanf(cipher_file, "%llu", &c) != EOF; i++) {
-		cipherMsg[i] = c;
-		if (i == numBlocks - 1) {
-			numBlocks *= 2;
-			temp = realloc(cipherMsg, numBlocks * sizeof(unsigned long long));
-			if (temp) {
-				cipherMsg = temp;
-			}
-			else {
-				fprintf(stderr, "[rsa_decrypt]: Realokacia pamate zlyhala.\n");
-				exit(1);
-			}
-		}
-	}
-	numBlocks = i;
-	temp = realloc(cipherMsg, numBlocks * sizeof(unsigned long long));
-	if (temp) {
-		cipherMsg = temp;
-	}
-	else {
-		fprintf(stderr, "[rsa_decrypt]: Realokacia pamate zlyhala.\n");
-		exit(1);
-	}
-	fclose(cipher_file);
+
+	begin = clock();
+	struct message decMsg;
 	for (i = 0; i < numBlocks - 1; i++) {
-		decrypted = rsa_modExp(cipherMsg[i], d, n);
+		decrypted[i] = rsa_modExp(decrypted[i], d, n);
 		//printf("Decrypted: %llu\n", decrypted);
-		decMsg = strToInt(bufSize, decrypted, 0);
+		decMsg = intToStr(bufSize, decrypted[i], 0);
 		fwrite(decMsg.msg, 1, decMsg.size, output_file);
+		free(decMsg.msg);
 	}
-	decrypted = rsa_modExp(cipherMsg[i], d, n);
-	decMsg = strToInt(bufSize, decrypted, 1);
+	decrypted[i] = rsa_modExp(decrypted[i], d, n);
+	decMsg = intToStr(bufSize, decrypted[i], 1);
 	fwrite(decMsg.msg, 1, decMsg.size, output_file);
+	end = clock();
+	time_spent = ((double)(end - begin) / CLOCKS_PER_SEC) * 1000;
+
 	if (output != NULL) {
 		fclose(output_file);
 	}
 	if (debug && output != NULL) {
-		printf("\nDesifrovanie dokoncene. Vystup je ulozeny v subore %s.\n", output);
+		printf("Desifrovanie trvalo %lf ms\n", time_spent);
+		printf("Desifrovanie dokoncene. Vystup je ulozeny v subore %s\n", output);
 	}
 	else if (debug) {
-		printf("\nDesifrovanie dokoncene. Vystup je na stdout.\n");
+		printf("\nDesifrovanie trvalo %lf ms\n", time_spent);
+		printf("Desifrovanie dokoncene. Vystup je na stdout\n");
 	}
 }
 
-unsigned char *inputString(char *input, long long bufSize, long long *numBlocks) {
+struct message inputString(char *input, long long bufSize) {
 	FILE *input_file;
+	struct message message;
 	int i = 0;
-	unsigned char c, *temp, *message, *tmpMsg;
-	long sizeOfFile = 0;
+	unsigned long long sizeOfFile;
 	//nacitanie suboru do buffra
 	if (input != NULL) {
 		input_file = fopen(input, "rb");
@@ -314,43 +363,51 @@ unsigned char *inputString(char *input, long long bufSize, long long *numBlocks)
 		//fseek(input_file, 0, SEEK_SET);
 		rewind(input_file);
 		if (debug) {
-			printf("Velkost suboru je:\t\t%ld\n", sizeOfFile);
+			printf("Velkost suboru je:\t\t%llu\n", sizeOfFile);
 		}
-		message = (unsigned char *)malloc(sizeof(unsigned char) * (sizeOfFile + 1));
-		int size = fread(message, 1, sizeOfFile, input_file);
-		message[sizeOfFile] = 255;
+		message.msg = (unsigned char *)malloc(sizeof(unsigned char) * (sizeOfFile + 1));
+		message.size = fread(message.msg, 1, sizeOfFile, input_file);
+		if (debug) {
+			printf("Nacitana velkost suboru je:\t%llu\n", message.size);
+		}
+		message.msg[sizeOfFile] = 255;
+		message.size++;
 		fclose(input_file);
 	}
-	//nacitanie znakov zo standardneho vstupu
+	//nacitanie suboru zo standardneho vstupu
 	else {
 		freopen(NULL, "rb", stdin);
 		input_file = stdin;
 		fseek(input_file, 0, SEEK_END);
 		sizeOfFile = ftell(input_file);
-		fseek(input_file, 0, SEEK_SET);
+		rewind(input_file);
 		if (sizeOfFile > 0) {
+			//citanie suboru zo stdin
 			if (debug) {
-				printf("Velkost suboru je:\t\t%ld\n", sizeOfFile);
+				printf("Velkost suboru je:\t\t%llu\n", sizeOfFile);
 			}
-			message = (unsigned char *)malloc(sizeof(unsigned char) * sizeOfFile);
-			sizeOfFile = fread(message, sizeof(unsigned char), sizeOfFile, input_file);
+			message.msg = (unsigned char *)malloc(sizeof(unsigned char) * sizeOfFile);
+			message.size = fread(message.msg, 1, sizeOfFile, input_file);
 			if (debug) {
-				printf("Nacitana velkost suboru je:\t%ld\n", sizeOfFile);
+				printf("Nacitana velkost suboru je:\t%llu\n", message.size);
 			}
-			message[sizeOfFile] = '\0';
+			message.msg[message.size++] = 255;
 		}
 		else {
-			message = (unsigned char *)malloc(sizeof(unsigned char) * 1024);
-			fgets(message, 1023, input_file);
-			sizeOfFile = strlen(message);
-			message[sizeOfFile - 1] = '\0';
+			//nacitanie znakov zo stdin
+			message.msg = (unsigned char *)malloc(sizeof(unsigned char) * (MAX_KEYBOARD_INPUT + 1));
+			fgets(message.msg, MAX_KEYBOARD_INPUT, input_file);
+			message.size = strlen(message.msg);
+			//message.msg[message.size - 1] = '\0';
+			message.msg[message.size++] = 255;
+			sizeOfFile = message.size;
 		}
 	}
 	if (sizeOfFile % bufSize != 0) {
-		*numBlocks = sizeOfFile / bufSize + 1;
+		message.numBlocks = sizeOfFile / bufSize + 1;
 	}
 	else {
-		*numBlocks = sizeOfFile / bufSize;
+		message.numBlocks = sizeOfFile / bufSize;
 	}
 	return message;
 }
@@ -369,8 +426,7 @@ int main(int argc, char **argv) {
 	struct public_key pub[1];
 	struct private_key priv[1];
 	int modSize = 32, bufSize = modSize / 8;
-	int i, j, flag = 0;
-	long long numBlocks = 0;
+	int i, flag = 0;
 	clock_t begin, end;
 	double time_spent;
 	char *output, *input;
@@ -464,13 +520,15 @@ int main(int argc, char **argv) {
 					}
 					i++;
 					if (argv[i] == NULL) {
-						if (debug && !strcmp(argv[i - 2], "-e")) {
+						if (debug) {
 							printf("Nebol zadany vstupny subor, citam stdin.\n");
 						}
 						input = NULL;
 						output = NULL;
 					}
 					else {
+						input = (char*)malloc(strlen(argv[i]) * sizeof(char));
+						strcpy(input, argv[i]);
 						if (argv[i + 1] != NULL) {
 							output = (char*)malloc(strlen(argv[i + 1]) * sizeof(char));
 							strcpy(output, argv[i + 1]);
@@ -481,49 +539,34 @@ int main(int argc, char **argv) {
 					}
 					
 					if (!strcmp(argv[i - 2], "-e")) {
-						unsigned char *message;
+						struct message message;
 						begin = clock();
 						//nacitanie vstupu do buffra
-						message = inputString(argv[i], bufSize, &numBlocks);
-						if (message == NULL) {
+						message = inputString(input, bufSize);
+						if (message.msg == NULL) {
 							fprintf(stderr, "Chyba pri citani zo vstupu!\n");
 							exit(1);
 						}
 						end = clock();
-						time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+						time_spent = (double)(end - begin) / CLOCKS_PER_SEC * 1000;
 						if (debug) {
-							printf("Nacitanie spravy zabralo %lf sekund.\n", time_spent);
-							printf("Pocet blokov: %llu\n", numBlocks);
+							printf("Nacitanie spravy zabralo %lf ms.\n", time_spent);
+							printf("Pocet blokov: %llu\n", message.numBlocks);
 						}
 						if (flag) {
-							rsa_encrypt(priv->d, priv->n, numBlocks, bufSize, message, output);
+							rsa_encrypt(priv->d, priv->n, bufSize, message, output);
 						}
 						else {
-							rsa_encrypt(pub->e, pub->n, numBlocks, bufSize, message, output);
+							rsa_encrypt(pub->e, pub->n, bufSize, message, output);
 						}
 						return 0;
 					}
 					else {
-						if (argv[i] != NULL) {
-							char *cipher;
-							cipher = (char*)malloc(strlen(argv[i]) * sizeof(char));
-							strcpy(cipher, argv[i]);
-							begin = clock();
-							if (flag) {
-								rsa_decrypt(priv->d, priv->n, bufSize, cipher, output);
-							}
-							else {
-								rsa_decrypt(pub->e, pub->n, bufSize, cipher, output);
-							}
-							end = clock();
-							time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-							if (debug) {
-								printf("Desifrovanie zabralo %lf sekund.\n", time_spent);
-							}
+						if (flag) {
+							rsa_decrypt(priv->d, priv->n, bufSize, input, output);
 						}
 						else {
-							fprintf(stderr, "Nebol zadany vstupny subor na desifrovanie.\n");
-							exit(1);
+							rsa_decrypt(pub->e, pub->n, bufSize, input, output);
 						}
 						return 0;
 					}
